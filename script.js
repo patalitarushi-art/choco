7c81423427d10c238e8ad370d14b1bb411dd62c7
patalitarushi-art <patalitarushi@gmail.com>
2026-02-09 23:03:54 +0530

    Simplify popup music playback handling
    Refactor music playback logic to enable on first click.

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-// POPUP FUNCTIONS
 function showPopup(id) {
     document.getElementById(id).style.display = "block";
-
-    // Try playing music on button click
     const music = document.getElementById("bgMusic");
-    music.volume = 1.0;
-    music.play().catch(() => {});
+    music.play().catch(()=>{});
 }
 
 function closePopup(id) {
     document.getElementById(id).style.display = "none";
 }
 
-// ENABLE MUSIC ON FIRST USER INTERACTION
-window.addEventListener("click", function enableAudio() {
+// Enable music on first click ANYWHERE
+window.addEventListener("click", () => {
     const music = document.getElementById("bgMusic");
-
-    music.play().then(() => {
-        console.log("Music started after user interaction");
-    }).catch(err => {
-        console.log("Autoplay still blocked:", err);
-    });
-
-    // Remove event listener after first successful play
-    window.removeEventListener("click", enableAudio);
-});
+    music.play().catch(()=>{});
+}, { once: true });
